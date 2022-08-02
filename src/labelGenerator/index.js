72df15b49771bbe67de2a444b1e2a1fc68408759
Dalya <dalya.ali@newcastle.ac.uk>
2022-08-02 16:21:41 +0100

    Fix label address lines wrapping

--- src/labelGenerator/index.js
+++ src/labelGenerator/index.js
@@ -30,11 +30,32 @@ export default {
       const svgElement = getLabelSvgElement();
       const addressData = data[i];
       console.log(addressData)
-      svgElement.querySelector("#shipping_address-name").textContent = addressData.get("name") || "";
-      svgElement.querySelector("#shipping_address-line1").textContent = addressData.get("addressLine1") || "";
-      svgElement.querySelector("#shipping_address-line2").textContent = addressData.get("addressLine2") || "";
-      svgElement.querySelector("#shipping_address-city").textContent = addressData.get("city") || "";
-      svgElement.querySelector("#shipping_address-postcode").textContent = addressData.get("postcode") || "";
+      const lineSpacing = 65;
+      const chunksRegex = /.{1,25}(\s|$)/g;
+      const addressToElementMap = {
+        "shipping_address-name": "name",
+        "shipping_address-line1": "addressLine1",
+        "shipping_address-line2": "addressLine2",
+        "shipping_address-city": "city",
+        "shipping_address-postcode": "postcode",
+      }
+      
+      let currentLineStart = 51;
+      for (let k in addressToElementMap) {
+        svgElement.querySelector(`#${k}`).textContent = "";
+        let chunks = (addressData.get(addressToElementMap[k]) || "").match(chunksRegex);
+        svgElement.querySelector(`#${k}`).setAttributeNS(null, "y", currentLineStart);
+        if (!chunks) continue;
+        chunks.forEach(c => {
+          let tspan = document.createElementNS("http://www.w3.org/2000/svg", "tspan");
+          tspan.textContent = c.toUpperCase();
+          tspan.setAttributeNS(null, "y", currentLineStart);
+          tspan.setAttributeNS(null, "x", 0);
+          currentLineStart += lineSpacing;
+          svgElement.querySelector(`#${k}`).appendChild(tspan)
+        })
+      }
+      
       let label = await api.saveAddressLabel({
         user: addressData.get("user"),
         address: [addressData.get("name") || "", addressData.get("addressLine1") || "", addressData.get("addressLine2") || "", addressData.get("city") || "", addressData.get("postcode") || ""].join('\n'),
@@ -48,6 +69,7 @@ export default {
       console.log(canv);
 
       svgElement.querySelector('#label_barcode').setAttributeNS('http://www.w3.org/1999/xlink','href', canv.toDataURL());
+      console.log(svgElement);
       await doc
         .svg(svgElement, {
           x: OFFSET_X,
@@ -67,12 +89,33 @@ export default {
         unit: "mm",
         format: [105, 148]
       });
+      const lineSpacing = 65;
+      const chunksRegex = /.{1,10}(\s|$)/g;
       const svgElement = getLabelSvgElement();
-      svgElement.querySelector("#shipping_address-name").textContent = addressData.get("name") || "";
-      svgElement.querySelector("#shipping_address-line1").textContent = addressData.get("addressLine1") || "";
-      svgElement.querySelector("#shipping_address-line2").textContent = addressData.get("addressLine2") || "";
-      svgElement.querySelector("#shipping_address-city").textContent = addressData.get("city") || "";
-      svgElement.querySelector("#shipping_address-postcode").textContent = addressData.get("postcode") || "";
+      const addressToElementMap = {
+        "shipping_address-name": "name",
+        "shipping_address-line1": "addressLine1",
+        "shipping_address-line2": "addressLine2",
+        "shipping_address-city": "city",
+        "shipping_address-postcode": "postcode",
+      }
+      
+      let currentLineStart = 51;
+      for (let k in addressToElementMap) {
+        svgElement.querySelector(`#${k}`).textContent = "";
+        let chunks = (addressData.get(addressToElementMap[k]) || "").match(chunksRegex);
+        svgElement.querySelector(`#${k}`).setAttributeNS(null, "y", currentLineStart);
+        if (!chunks) continue;
+        chunks.forEach(c => {
+          let tspan = document.createElementNS("http://www.w3.org/2000/svg", "tspan");
+          tspan.textContent = c.toUpperCase();
+          tspan.setAttributeNS(null, "y", currentLineStart);
+          tspan.setAttributeNS(null, "x", 0);
+          currentLineStart += lineSpacing;
+          svgElement.querySelector(`#${k}`).appendChild(tspan)
+        })
+      }
+      
       svgElement.querySelector("#box_number").textContent = boxNumber;
 
       let label = await api.saveAddressLabel({
